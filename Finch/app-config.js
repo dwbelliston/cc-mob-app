afb76210932ffeeee68d8d323caebba0e5622497
Dustin Belliston <dustinbelliston@gmail.com>
2023-08-29 11:57:03 -0600

    v 0.9.2

--- Finch/app-config.js
+++ Finch/app-config.js
@@ -12,7 +12,7 @@ import {
 import { APIEndpoints } from "./app/services/api/config"
 
 const appConfig = {
-  version: "0.9.1 (Beta)",
+  version: "0.9.2 (Beta)",
   env: REACT_APP_ENV,
   isDev: REACT_APP_ENV === "DEV",
   Auth: {
