047a5db80c45e4e1d7a678665fc0f0ed39adfc24
Dustin Belliston <dustinbelliston@gmail.com>
2023-12-27 13:12:44 -0700

    update render message with contact

--- Finch/app/utils/useFormatMessage.tsx
+++ Finch/app/utils/useFormatMessage.tsx
@@ -9,13 +9,18 @@ export const renderMessageWithContact = (
   let renderedMessageBody = message
 
   // Parse text to replace first and last name
-  renderedMessageBody = renderedMessageBody.replace(/\${FirstName}/g, firstName)
+  renderedMessageBody = renderedMessageBody.replace(/\${firstname}/gi, firstName)
+  renderedMessageBody = renderedMessageBody.replace(/\${fname}/gi, firstName)
+  renderedMessageBody = renderedMessageBody.replace(/\${first}/gi, firstName)
 
   const nicknameFallback = nickname || firstName
 
-  renderedMessageBody = renderedMessageBody.replace(/\${Nickname}/g, nicknameFallback)
+  renderedMessageBody = renderedMessageBody.replace(/\${nickname}/gi, nicknameFallback)
+  renderedMessageBody = renderedMessageBody.replace(/\${nick}/gi, nicknameFallback)
 
-  renderedMessageBody = renderedMessageBody.replace(/\${LastName}/g, lastName)
+  renderedMessageBody = renderedMessageBody.replace(/\${lastname}/gi, lastName)
+  renderedMessageBody = renderedMessageBody.replace(/\${lname}/gi, lastName)
+  renderedMessageBody = renderedMessageBody.replace(/\${last}/gi, lastName)
 
   return renderedMessageBody
 }
